33fe38c226d4f617326468c48bbb486b98fae047
ahmadrawash <ahmedyr2003@gmail.com>
2022-04-23 06:16:39 -0500

    WebsiteExample3

---
+++ WebsiteExample3/WebsiteExample3/src/main/java/WebsiteExample3/WebsiteExample3/WebsiteExample3Application.java
@@ -0,0 +1,14 @@
+package WebsiteExample3.WebsiteExample3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteExample3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteExample3Application.class, args);
+		System.out.println("testing");
+	}
+
+}
---
+++ WebsiteExample3/WebsiteExample3/src/test/java/WebsiteExample3/WebsiteExample3/WebsiteExample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package WebsiteExample3.WebsiteExample3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteExample3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
