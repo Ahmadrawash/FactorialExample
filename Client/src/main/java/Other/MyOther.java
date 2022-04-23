504a8362b417bd1739ffdbfd2d81c6371a6eda6e
ahmadrawash <ahmedyr2003@gmail.com>
2022-04-23 05:37:15 -0500

    Client

---
+++ Client/src/main/java/Other/MyOther.java
@@ -0,0 +1,10 @@
+//package Other;
+
+public class MyOther {
+    public MyOther()
+    {
+        MyTesting mt = new MyTesting();
+
+    }
+}
+
---
+++ Client/src/main/java/Services/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[])  {
+        try {
+
+            //MyJDBC.AddUser("Ali", "123", "Ali@refvature.net");
+            MyJDBC.DisplayData();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
