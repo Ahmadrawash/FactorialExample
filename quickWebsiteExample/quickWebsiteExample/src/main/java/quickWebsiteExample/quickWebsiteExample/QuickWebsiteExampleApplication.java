92d356aeba1e2e22cbaffb442c6530ab313702ba
ahmadrawash <ahmedyr2003@gmail.com>
2022-04-23 05:59:48 -0500

    quickWebsiteExample

---
+++ quickWebsiteExample/quickWebsiteExample/src/main/java/quickWebsiteExample/quickWebsiteExample/QuickWebsiteExampleApplication.java
@@ -0,0 +1,14 @@
+package quickWebsiteExample.quickWebsiteExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickWebsiteExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickWebsiteExampleApplication.class, args);
+		System.out.println("testing");
+	}
+
+}
---
+++ quickWebsiteExample/quickWebsiteExample/src/test/java/quickWebsiteExample/quickWebsiteExample/QuickWebsiteExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package quickWebsiteExample.quickWebsiteExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickWebsiteExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
