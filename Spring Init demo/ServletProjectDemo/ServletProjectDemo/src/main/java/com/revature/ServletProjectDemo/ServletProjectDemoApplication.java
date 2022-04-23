1a315e8bba510c26753704a2b8b68b94f7c0c85c
ahmadrawash <ahmedyr2003@gmail.com>
2022-04-23 06:06:31 -0500

    Spring

---
+++ Spring Init demo/ServletProjectDemo/ServletProjectDemo/src/main/java/com/revature/ServletProjectDemo/ServletProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ServletProjectDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletProjectDemoApplication.class, args);
+	}
+
+}
---
+++ Spring Init demo/ServletProjectDemo/ServletProjectDemo/src/test/java/com/revature/ServletProjectDemo/ServletProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ServletProjectDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
