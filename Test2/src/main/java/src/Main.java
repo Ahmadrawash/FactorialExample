cf336f33317a3794dcb9d4157f64a9e32d8f847e
ahmadrawash <ahmedyr2003@gmail.com>
2022-04-23 06:11:55 -0500

    Test2

---
+++ Test2/src/main/java/src/Main.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Main {
+    public static void main(String args[])
+    {
+        //Thread thread = new Thread(new myThread());
+//        Thread thread = new Thread(new MyThread());
+//        System.out.println(thread.getPriority());
+//        System.out.println("printing");
+
+        int[][] array = new int[5][];
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        System.out.println(arr[4]);
+
+    }
+}
+
---
+++ Test2/src/main/java/src/MyThread.java
@@ -0,0 +1,8 @@
+package src;
+
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Inside the src.MyThread class");
+    }
+}
